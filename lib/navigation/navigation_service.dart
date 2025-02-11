ceb91f5646fa25c8e5f7794d606952c2a6369757
syaha-creator <msyahrul090@gmail.com>
2025-02-11 11:47:23 +0700

    Add Navigation Page

--- lib/navigation/navigation_service.dart
+++ lib/navigation/navigation_service.dart
@@ -5,12 +5,17 @@ import 'app_router.dart';
 class NavigationService {
   static final GlobalKey<NavigatorState> navigatorKey =
       GlobalKey<NavigatorState>();
+
   static void navigateTo(String routeName, {Map<String, String>? params}) {
     if (params != null) {
       AppRouter.router.goNamed(routeName, pathParameters: params);
     } else {
       AppRouter.router.go(routeName);
     }
+  }
+
+  static void navigateAndReplace(String routeName) {
+    AppRouter.router.replace(routeName);
   }
 
   static void goBack() {
--- lib/navigation/services/auth_service.dart
+++ lib/navigation/services/auth_service.dart
@@ -3,16 +3,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AuthService {
   static const String _isLoggedInKey = "is_logged_in";
   static const String _loginTimestampKey = "login_timestamp";
+  static const String _tokenKey = "auth_token";
   static const int _sessionDuration = 24 * 60 * 60 * 1000;
 
   static Future<bool> isLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
     bool isLoggedIn = prefs.getBool(_isLoggedInKey) ?? false;
     int? loginTimestamp = prefs.getInt(_loginTimestampKey);
+    String? token = prefs.getString(_tokenKey);
 
-    if (isLoggedIn) {
+    if (isLoggedIn && loginTimestamp != null) {
       int currentTime = DateTime.now().millisecondsSinceEpoch;
-      if (currentTime - loginTimestamp! > _sessionDuration) {
+      if (currentTime - loginTimestamp > _sessionDuration || token == null) {
         await logout();
         return false;
       }
@@ -20,16 +22,18 @@ class AuthService {
     return isLoggedIn;
   }
 
-  static Future<void> login() async {
+  static Future<void> login(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(_isLoggedInKey, true);
     await prefs.setInt(
         _loginTimestampKey, DateTime.now().millisecondsSinceEpoch);
+    await prefs.setString(_tokenKey, token);
   }
 
   static Future<void> logout() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_isLoggedInKey);
     await prefs.remove(_loginTimestampKey);
+    await prefs.remove(_tokenKey);
   }
 }
