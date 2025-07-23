0e4f995af3e7125d721bd82193f12db793e54ed8
Syaha Creator <msyahrul090@gmail.com>
2025-07-23 21:49:06 +0700

    Refactor(core/utils): Remove unused bloc_disposal_mixin and clean up controller_disposal_mixin to only keep used functions. Improves maintainability and reduces dead code.

--- lib/core/utils/controller_disposal_mixin.dart
+++ lib/core/utils/controller_disposal_mixin.dart
@@ -6,9 +6,6 @@ import 'package:flutter/material.dart';
 mixin ControllerDisposalMixin<T extends StatefulWidget> on State<T> {
   final List<TextEditingController> _controllers = [];
   final List<FocusNode> _focusNodes = [];
-  final List<ScrollController> _scrollControllers = [];
-  final List<AnimationController> _animationControllers = [];
-  final List<PageController> _pageControllers = [];
 
   /// Register TextEditingController untuk auto-disposal
   TextEditingController registerController([String? initialText]) {
@@ -24,59 +21,6 @@ mixin ControllerDisposalMixin<T extends StatefulWidget> on State<T> {
     return focusNode;
   }
 
-  /// Register ScrollController untuk auto-disposal
-  ScrollController registerScrollController() {
-    final scrollController = ScrollController();
-    _scrollControllers.add(scrollController);
-    return scrollController;
-  }
-
-  /// Register AnimationController untuk auto-disposal
-  AnimationController registerAnimationController({
-    required TickerProvider vsync,
-    required Duration duration,
-    Duration? reverseDuration,
-  }) {
-    final animationController = AnimationController(
-      vsync: vsync,
-      duration: duration,
-      reverseDuration: reverseDuration,
-    );
-    _animationControllers.add(animationController);
-    return animationController;
-  }
-
-  /// Register PageController untuk auto-disposal
-  PageController registerPageController({
-    int initialPage = 0,
-    bool keepPage = true,
-    double viewportFraction = 1.0,
-  }) {
-    final pageController = PageController(
-      initialPage: initialPage,
-      keepPage: keepPage,
-      viewportFraction: viewportFraction,
-    );
-    _pageControllers.add(pageController);
-    return pageController;
-  }
-
-  /// Manual disposal untuk controller tertentu
-  void disposeController(TextEditingController controller) {
-    if (_controllers.contains(controller)) {
-      _controllers.remove(controller);
-      controller.dispose();
-    }
-  }
-
-  /// Manual disposal untuk focus node tertentu
-  void disposeFocusNode(FocusNode focusNode) {
-    if (_focusNodes.contains(focusNode)) {
-      _focusNodes.remove(focusNode);
-      focusNode.dispose();
-    }
-  }
-
   @override
   void dispose() {
     // Dispose all registered controllers
@@ -90,24 +34,6 @@ mixin ControllerDisposalMixin<T extends StatefulWidget> on State<T> {
       focusNode.dispose();
     }
     _focusNodes.clear();
-
-    // Dispose all registered scroll controllers
-    for (final scrollController in _scrollControllers) {
-      scrollController.dispose();
-    }
-    _scrollControllers.clear();
-
-    // Dispose all registered animation controllers
-    for (final animationController in _animationControllers) {
-      animationController.dispose();
-    }
-    _animationControllers.clear();
-
-    // Dispose all registered page controllers
-    for (final pageController in _pageControllers) {
-      pageController.dispose();
-    }
-    _pageControllers.clear();
 
     super.dispose();
   }
